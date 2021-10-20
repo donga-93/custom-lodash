415d2994f02606ed0fda4a6e715ae213e431385c
donga-93 <zazadongadz2@gmail.com>
2021-10-20 16:20:44 +0400

    feat: structure

--- src/chunk.js
+++ src/chunk.js
@@ -1,5 +1,17 @@
-function chunk(array, n = 1) {
-    return [];
+function chunk(array, size = 1) {
+    let newArray = [];
+        let length = Math.ceil(array.length / size);
+        for (let i = 0; i < length; i++){
+            newArray[i] = [];
+            for (let j = 0; j < size; j++) {
+                if (i * size + j >= array.length){
+                    break;
+                }
+
+                newArray[i][j] = array[i*size + j];
+            }
+        }
+        return newArray;
 }
 
 module.exports = chunk
--- test/drop.test.js
+++ test/drop.test.js
@@ -1,1 +1,5 @@
 const drop = require('../src/drop');
+
+drop('saxeli', ()=>{
+    expect([1, 2, 3]).toStrictEqual([2, 3]);
+})
